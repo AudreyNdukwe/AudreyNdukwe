57027845dbb57f7627691be7f2d9788544b44a93
AudreyNdukwe <89554316+AudreyNdukwe@users.noreply.github.com>
2021-08-26 21:06:51 +1000

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,13 @@
+//creates an alert
+alert('Hello World!');
+/* This is a example 
+of a multi line comment */
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function openMenu() {
+var x = document.getElementById("portfoliotopnav");
+if (x.className === "topnav") {
+    x.className += "responsive";
+ } else {
+    x.className ="topnav";
+ }
+}
